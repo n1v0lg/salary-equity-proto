5466587d55b02fd0034740e2bdf8b27e090d6e10
Nikolaj <nikolaj@nina-II.(none)>
2016-05-24 13:28:33 -0400

    Forgot file.

---
+++ twistedutil.py
@@ -0,0 +1,70 @@
+from twisted.internet.ssl import ClientContextFactory
+from twisted.internet.protocol import Factory, Protocol
+from twisted.web import resource
+from twisted.web.server import NOT_DONE_YET
+import jsonpickle
+
+class WebClientContextFactory(ClientContextFactory):
+    def getContext(self, hostname, port):
+        return ClientContextFactory.getContext(self)
+
+class ConfigReader(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.data = ''
+        self.remaining = 1024 * 10
+
+    def dataReceived(self, bytes):
+        if self.remaining:
+            display = bytes[:self.remaining]
+            self.data += str(display)
+            self.remaining -= len(display)
+
+    def connectionLost(self, reason):
+        self.finished.callback(self.data)
+
+class DataEntryForm(resource.Resource):
+    isLeaf = True
+
+    def __init__(self, html):
+        resource.Resource.__init__(self)
+        self.html = html
+
+    def render_GET(self, request):
+        # figure out the twisted way to do this
+        return self.html
+
+class DataEndpoint(resource.Resource):
+    isLeaf = True
+
+    def __init__(self, repo, session, peer_origins):
+        resource.Resource.__init__(self)
+        self.peer_origins = peer_origins
+        self.repo = repo
+        self.session = session
+
+    def render_POST(self, request):
+        origin = request.getHeader('origin')
+        print origin
+        if origin in self.peer_origins:
+            request.setHeader('Access-Control-Allow-Origin', origin)
+            request.setHeader('Access-Control-Allow-Methods', 'POST')
+        
+        raw = request.content.getvalue()
+        data = jsonpickle.decode(raw) # outrageously insecure
+        self.repo.add_share(data)
+        self.session.input_parties.discard(data[0])
+        request.write('{}') 
+        request.finish()
+    
+        return NOT_DONE_YET
+
+class MPCDetails(resource.Resource):
+    isLeaf = True
+
+    def __init__(self, mpc_details):
+        resource.Resource.__init__(self)
+        self.mpc_details = jsonpickle.encode(mpc_details)
+
+    def render_GET(self, request):
+        return self.mpc_details
